17c58eb92fd54afadc352ad93cd01075cb1530e9
Elliana Schmitt <eroseschmitt@gmail.com>
2020-07-28 13:52:30 -0400

    initial commit

---
+++ Mini Project 1/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Mini Project 1
+//
+//  Created by Apple on 7/22/20.
+//  Copyright © 2020 Elliana Schmitt. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var abtTitle: UILabel!
+    
+    @IBOutlet weak var togetherPic: UIImageView!
+    
+    @IBOutlet weak var catPic: UIImageView!
+    
+    @IBOutlet weak var arrowButton: UITextField!
+    
+    @IBOutlet weak var aboutmeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("View has loaded. :)")
+    }
+
+
+}
+
---
+++ Mini Project 1/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  Mini Project 1
+//
+//  Created by Apple on 7/23/20.
+//  Copyright © 2020 Elliana Schmitt. All rights reserved.
+//
+
+import UIKit
+
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var factLabel: UILabel!
+    
+    @IBOutlet weak var catPic: UIImageView!
+    
+    @IBOutlet weak var fact1: UITextField!
+    
+    @IBOutlet weak var fact2: UITextField!
+    
+    @IBOutlet weak var fact3: UITextField!
+    
+    @IBOutlet weak var faveEmoji: UILabel!
+    override func viewDidLoad() {
+        // Do any additional setup after loading the view.
+    }
+
+
+}
